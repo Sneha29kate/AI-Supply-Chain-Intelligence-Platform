92b07793f3d03adc31367f88237e8ba3763d13a8
Sneha Kate <snehakate15@gmail.com>
2026-06-13 16:06:57 +0530

    Fix analytics and weather

--- app/app.py
+++ app/app.py
@@ -19,6 +19,9 @@ from database.fetch_shipments import get_shipments
 from forecasting.demand_forecasting import forecast_demand
 from weather.weather_api import get_weather
 from database.save_weather import save_weather
+# from database.save_shipment import save_shipment
+# from database.fetch_shipments import get_shipments
+# from database.save_weather import save_weather
 st.set_page_config(
     page_title="AI Supply Chain Intelligence Platform",
     layout="wide"
@@ -111,16 +114,16 @@ if page == "Shipment Delay Prediction":
             delayed_value = 1
 
         save_shipment(
-            distance=distance,
-            warehouse=str(warehouse),
-            shipping_mode=str(shipping_mode),
-            priority=str(priority),
-            delayed=delayed_value
-        )
+    distance=distance,
+    warehouse=str(warehouse),
+    shipping_mode=str(shipping_mode),
+    priority=str(priority),
+    delayed=delayed_value
+)
 
-        st.success(
-            "✅ Record saved to PostgreSQL"
-        )
+st.success(
+    "✅ Record saved to PostgreSQL"
+)
 
         if hasattr(model, "predict_proba"):
 
